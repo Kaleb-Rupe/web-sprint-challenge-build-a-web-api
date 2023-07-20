cecec3f8b6c5336ea8cddb84b5d93be0cb11f0ae
Kaleb-Rupe <57547999+Kaleb-Rupe@users.noreply.github.com>
2023-07-20 12:42:24 -0400

    Server is setup pre install of router & middleware

--- api/projects/projects-middleware.js
+++ api/projects/projects-middleware.js
@@ -1,1 +1,16 @@
+const Projects = require("./projects-model");
+
 // add middlewares here related to projects
+
+function projectsLogger(req, res, next) {
+  // DO YOUR MAGIC
+  const timestamp = new Date().toLocaleDateString();
+  const method = req.method;
+  const url = req.originalUrl;
+  console.log(`[${timestamp}] ${method} to ${url}`);
+  next();
+}
+
+module.exports = {
+  projectsLogger,
+};
--- api/server.js
+++ api/server.js
@@ -1,5 +1,22 @@
-const express = require('express');
+const express = require("express");
+// const actionsRouter = require("./actions/actions-router");
+// const projectsRouter = require("./projects/projects-router");
+// const { actionsLogger } = require("./actions/actions-middlware");
+// const { projectsLogger } = require("./projects/projects-middleware");
+
 const server = express();
+
+server.use(express.json());
+// server.use("./actions", actionsRouter);
+// server.use("./projects", projectsRouter);
+
+// server.use(actionsLogger);
+// server.use(projectsLogger);
+
+server.use("*", (req, res) => {
+  // catch all 404 errors middleware
+  res.status(404).json({ message: `${req.method} ${req.baseUrl} not found` });
+});
 
 // Configure your server here
 // Build your actions router in /api/actions/actions-router.js
