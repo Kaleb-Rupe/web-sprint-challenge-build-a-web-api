0fa6b20104dd5ef10fd6a95025e7920de863015b
Kaleb-Rupe <57547999+Kaleb-Rupe@users.noreply.github.com>
2023-07-20 12:42:21 -0400

    Server is setup pre install of router & middleware

--- api/actions/actions-middlware.js
+++ api/actions/actions-middlware.js
@@ -1,1 +1,14 @@
 // add middlewares here related to actions
+
+function actionsLogger(req, res, next) {
+  // DO YOUR MAGIC
+  const timestamp = new Date().toLocaleDateString();
+  const method = req.method;
+  const url = req.originalUrl;
+  console.log(`[${timestamp}] ${method} to ${url}`);
+  next();
+}
+
+module.exports = {
+  actionsLogger,
+};
